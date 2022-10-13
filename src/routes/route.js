f3de285949ff61b6daa3ab4366454b0ffbfe3959
shukla20 <sonarshukla@gmail.com>
2022-10-13 18:27:11 +0530

    Hello

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
-
+router.get('/test-you', function (req, res) {
+    res.send('My second ever api!')
+});
 module.exports = router;
